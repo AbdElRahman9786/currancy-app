c70ed76739b62be06eb3173d1a832bba32951dc3
unknown <bodaahmed2500@gmail.com>
2024-08-13 17:17:55 +0300

    handel js bug

--- main.js
+++ main.js
@@ -5,37 +5,34 @@ let inpt3=document.getElementById("inpt3");
 let btn=document.getElementById("btn");
 
 
- fetch(
-  "https://api.currencyfreaks.com/v2.0/rates/latest?apikey=99bddac1008649a2a26c65ad3d4f8190"
-)
-  .then( (res) => {
-return  res.json();
-  }).then(async (data) => {
-    inpt.addEventListener("input", async (event) => {
-      let string= event.target.value;
-      let cstring=string.toUpperCase();
-  await inpt2.addEventListener("input", (event) =>{ 
-    btn.addEventListener("click",()=>{
-      if(!isNaN(data.rates[cstring] * event.target.value)){
-       inpt3.value= (data.rates[cstring] * event.target.value).toFixed(2)
-      }else{
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "you type wrong data",
-          
-        });
-      }
-      })
-    
-  
-  })
-   
-     })
-     
-  })
+let globalData; // Global variable to store the fetched data
 
-  
+async function getData(url) {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data.rates;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
+async function initializeData() {
+  globalData = await getData("https://api.currencyfreaks.com/v2.0/rates/latest?apikey=99bddac1008649a2a26c65ad3d4f8190");
 
- 
+}
+initializeData();
+
+btn.addEventListener("click",()=>{
+  let string= inpt.value;
+     let cstring=string.toUpperCase();
+     if(!isNaN(globalData[cstring] * inpt2.value)){
+  inpt3.value = (globalData[cstring] * inpt2.value).toFixed(2);
+     }else{
+      Swal.fire({
+                    icon: "error",
+                     title: "Oops...",
+                   text: "you type wrong data",
+                });
+     }
+})
